4a63dc1df29e47d1c93f9bb08ae866f4c149247e
y-natani <yn1323@gmail.com>
2025-04-13 00:16:52 +0900

    fix: storybook files

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,9 +1,9 @@
 import type { StorybookConfig } from "@storybook/experimental-nextjs-vite";
+import path from "node:path";
 
 const config: StorybookConfig = {
   "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+    "../src/**/*.stories.@(ts|tsx)"
   ],
   "addons": [{
     "name": "@storybook/addon-essentials",
@@ -15,8 +15,31 @@ const config: StorybookConfig = {
     "name": "@storybook/experimental-nextjs-vite",
     "options": {}
   },
-  "staticDirs": [
-    "..\\public"
-  ]
+  typescript: {
+    reactDocgen:
+      process.env.NODE_ENV === 'development'
+        ? 'react-docgen'
+        : 'react-docgen-typescript',
+  },
+  staticDirs: ["../public"],
+  viteFinal: async (config) => {
+    if (!config.resolve) {
+      return config;
+    }
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@/app': path.resolve(__dirname, '../app'),
+      '@/prisma': path.resolve(__dirname, '../prisma'),
+      '@/src': path.resolve(__dirname, '../src'),
+      '@/e2e': path.resolve(__dirname, '../e2e'),
+    };
+    return config;
+  },
+  env: (config) => ({
+    ...config,
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL ?? '',
+    NEXT_PUBLIC_SUPABASE_ANON_KEY:
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? '',
+  }),
 };
 export default config;
