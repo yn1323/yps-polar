314a1b179816f7b7dc6a701050be0daf4d79263f
y-natani <yn1323@gmail.com>
2025-04-29 20:15:55 +0900

    サインインフォームのリンク先ページを作成

---
+++ app/login/forget/page.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import { Box, Center, Heading, Text } from '@chakra-ui/react';
+
+export default function ForgetPasswordPage() {
+  return (
+    <Center h="100vh">
+      <Box textAlign="center">
+        <Heading as="h1" size="xl" mb={4}>
+          パスワードをお忘れの方
+        </Heading>
+        <Text>ここにパスワードリセットフォームが表示されます。</Text>
+      </Box>
+    </Center>
+  );
+}
---
+++ app/login/signup/page.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import { Box, Center, Heading, Text } from '@chakra-ui/react';
+
+export default function SignupPage() {
+  return (
+    <Center h="100vh">
+      <Box textAlign="center">
+        <Heading as="h1" size="xl" mb={4}>
+          新規登録ページ
+        </Heading>
+        <Text>ここに新規登録フォームが表示されます。</Text>
+      </Box>
+    </Center>
+  );
+}
