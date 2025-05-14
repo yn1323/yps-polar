1b4ecae60275b5b3f3434f80231d1a93f3f261bd
y-natani <yn1323@gmail.com>
2025-05-15 01:41:10 +0900

    fix: signupform

--- src/components/features/signin/SignupForm/index.tsx
+++ src/components/features/signin/SignupForm/index.tsx
@@ -7,22 +7,34 @@ export const SignupForm = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isSubmitting },
   } = useForm<SchemaType>({
     resolver: zodResolver(schema),
   });
 
-  const onSubmit: SubmitHandler<SchemaType> = (data) => console.log(data);
+  const onSubmit: SubmitHandler<SchemaType> = async (data) => {
+    // wait 3 seconds
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log(data);
+  };
 
   return (
     <Card.Root w="96" p="8">
       <Stack gap="8" w="full">
-        <Stack gap="6" as="form" onSubmit={handleSubmit(onSubmit)}>
+        <Stack
+          gap="6"
+          as="form"
+          onSubmit={(e) => {
+            e.preventDefault();
+            handleSubmit(onSubmit)(e);
+          }}
+        >
           <Field.Root invalid={!!errors.mail}>
             <Field.Label>メールアドレス</Field.Label>
             <Input
               {...register('mail')}
               placeholder="メールアドレスを入力してください"
+              disabled={isSubmitting}
             />
             <Field.ErrorText>{errors.mail?.message}</Field.ErrorText>
           </Field.Root>
@@ -33,6 +45,7 @@ export const SignupForm = () => {
               {...register('password')}
               type="password"
               placeholder="パスワードを入力してください"
+              disabled={isSubmitting}
             />
             <Field.ErrorText>{errors.password?.message}</Field.ErrorText>
           </Field.Root>
@@ -43,13 +56,19 @@ export const SignupForm = () => {
               {...register('passwordConfirmation')}
               type="password"
               placeholder="パスワードを入力してください（確認用）"
+              disabled={isSubmitting}
             />
             <Field.ErrorText>
               {errors.passwordConfirmation?.message}
             </Field.ErrorText>
           </Field.Root>
 
-          <Button variant="solid" colorPalette="teal" type="submit">
+          <Button
+            variant="solid"
+            colorPalette="teal"
+            type="submit"
+            loading={isSubmitting}
+          >
             登録
           </Button>
         </Stack>
